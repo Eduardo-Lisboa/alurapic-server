1f9a49c63739f77708f46ca4fad30dd33f1536af
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2023-02-06 23:02:02 -0300

    add res

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Param } from "@nestjs/common";
+import { Body, Controller, Post, Get, Param, Res, HttpStatus } from "@nestjs/common";
 import { UsuarioService } from "./usuario.service";
 import { Usuario } from "./usuario.entity";
 
@@ -7,16 +7,15 @@ export class UsuarioController {
     constructor(private usuarioService: UsuarioService) {}
 
     @Get(":nomeDeUsuario")
-    public buscaPorNomeDeUsuario(@Param("nomeDeUsuario") nomeDeUsuario: string): Usuario {
+    public buscaPorNomeDeUsuario(@Param("nomeDeU suario") nomeDeUsuario: string): Usuario {
         const usuario = this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario);
 
         return usuario;
     }
 
     @Post()
-    public cria(@Body() usuario: Usuario): Usuario {
+    public cria(@Body() usuario: Usuario, @Res() res) {
         const usuarioCriado = this.usuarioService.cria(usuario);
-
-        return usuarioCriado;
+        res.status(HttpStatus.CREATED).location(`/users/${usuarioCriado.nomeDeUsuario}`).json(usuarioCriado);
     }
 }
